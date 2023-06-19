501f5c2e860a2c08882e4e82392066ad9fb680c8
rishiraj007 <rishiraj@mounttalent.com>
2023-06-19 19:14:06 +0530

    [Prabhu]Add: create MyMapNode with Key Value Pair and create LinkedList of MyMapNode

--- HashTableAndBST/Program.cs
+++ HashTableAndBST/Program.cs
@@ -5,6 +5,39 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hash Table And BST !");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Hash Table \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        hash.Add("0", "To");
+                        hash.Add("1", "be");
+                        hash.Add("2", "or");
+                        hash.Add("3", "not");
+                        hash.Add("4", "to");
+                        hash.Add("5", "be");
+                        string hash0 = hash.Get("0");
+                        string hash1 = hash.Get("1");
+                        string hash2 = hash.Get("2");
+                        string hash3 = hash.Get("3");
+                        string hash4 = hash.Get("4");
+                        string hash5 = hash.Get("5");
+                        Console.WriteLine("0th index value: " + hash0);
+                        Console.WriteLine("1th index value: " + hash1);
+                        Console.WriteLine("2nd index value: " + hash2);
+                        Console.WriteLine("3rd index value: " + hash3);
+                        Console.WriteLine("4th index value: " + hash4);
+                        Console.WriteLine("5th index value: " + hash5);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
